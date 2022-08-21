240681d168bc617fb1cfe4cb35e60f10b417da3e
Fabin <fsfabianosantos03@gmail.com>
2022-08-20 21:00:02 -0300

    version1

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react';
+import Switch from 'react-switch';
+import { ThemeContext } from 'styled-components';
+import { shade } from 'polished';
+
+import { Container } from './style';
+
+interface Props {
+  toggleTheme(): void;
+}
+
+const Header: React.FC<Props> = ({ toggleTheme }) => {
+  const { colors, title } = useContext(ThemeContext);
+
+  return (
+    <Container>
+      Hello World
+      <Switch
+        onChange={toggleTheme}
+        checked={title === 'dark'}
+        checkedIcon={false}
+        uncheckedIcon={false}
+        height={10}
+        width={40}
+        handleDiameter={20}
+        offColor={shade(0.2, colors.primary)}
+        onColor={colors.secondary}
+      ></Switch>
+    </Container>
+  );
+};
+export default Header;
---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,11 @@
+export default {
+    title: 'dark',
+    //usaremos isso para ter uma base de qual tema iremos usar
+    //ao invés de usar pelo nome do arquivo
+    colors: {
+        primary: '#0F0f0f',
+        secondary: '#2F4550',
+        background: '#222',
+        text: '#fff',
+    },
+};
